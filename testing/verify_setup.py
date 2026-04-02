134d52f9f68980ae24107225a7839db15fa57e55
unknown <ahmedmakkass12@gmail.com>
2026-04-02 13:54:54 +0300

    added a whole lotta of stuff, changed the verify_setup so that it now streams the answer, and it's inside a loop

---
+++ testing/verify_setup.py
@@ -0,0 +1,22 @@
+import os
+from openai import OpenAI
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1",
+)
+while True:
+    userInput = input("Listening: ")
+    if userInput.lower() in ["quit", "q", "exit", "stop"]:
+        break
+    stream = client.responses.create(
+        model="llama-3.3-70b-versatile",
+        instructions="ONLY answer Python-related questions.",
+        input=f"{userInput}",
+        stream=True,
+    )
+    print()
+    print("AI: ", end="", flush=True)
+    for word in stream:
+        if word.type == "response.output_text.delta":
+            print(word.delta, end="", flush=True)
+    print()
